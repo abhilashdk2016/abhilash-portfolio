cd80b0b9c0b684c67c5aa59c506d5cb164e3f402
abhilashdk2016 <abhilashdk2014@outlook.com>
2025-02-02 04:05:20 +0530

    My Portfolio

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import {
   RxDiscordLogo,
@@ -17,7 +18,9 @@ const Footer = () => {
                     <div className="font-bold text-[16px]">Community</div>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                         <RxGithubLogo />
-                        <span className="text-[15px] ml-[6px]">Github</span>    
+                        <Link href={"https://github.com/abhilashdk2016"} target="_blank">
+                            <span className="text-[15px] ml-[6px]">Github</span>   
+                        </Link>  
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                         <RxDiscordLogo />
@@ -28,17 +31,20 @@ const Footer = () => {
                     <div className="font-bold text-[16px]">Social Media</div>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                         <RxTwitterLogo />
-                        <span className="text-[15px] ml-[6px]">Twitter</span>    
+                        <Link href={"https://x.com/AbhilashDK2022"} target="_blank"> 
+                            <span className="text-[15px] ml-[6px]">Twitter</span>   
+                        </Link> 
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                         <RxLinkedinLogo />
-                        <span className="text-[15px] ml-[6px]">Linkedin</span>    
+                        <Link href={"https://www.linkedin.com/in/abhilashdk"} target="_blank"> 
+                            <span className="text-[15px] ml-[6px]">Linkedin</span>
+                        </Link>
                     </p>
                 </div>
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
                     <div className="font-bold text-[16px]">About</div>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                  
                         <span className="text-[15px] ml-[6px]">abhilashdk2018@outlook.com</span>    
                     </p>
                 </div>
